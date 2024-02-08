be547f244647006bb7a7a3d21b007a5afaf7a68b
Jesse de Wild <jessedw@outlook.com>
2024-02-08 15:16:29 +0100

    Other mode and options

--- src/ios/CDVSound.m
+++ src/ios/CDVSound.m
@@ -438,9 +438,9 @@ BOOL keepAvAudioSessionAlwaysActive = NO;
                     bPlayAudioWhenScreenIsLocked = [playAudioWhenScreenIsLocked boolValue];
                 }
 
-                NSString* sessionCategory = bPlayAudioWhenScreenIsLocked ? AVAudioSessionCategoryPlayAndRecord : AVAudioSessionCategorySoloAmbient;
-                NSString* sessionMode = AVAudioSessionModeVideoRecording;
-                AVAudioSessionCategoryOptions options = AVAudioSessionCategoryOptionDefaultToSpeaker | AVAudioSessionCategoryOptionAllowBluetooth | AVAudioSessionCategoryOptionMixWithOthers;
+                NSString* sessionCategory = AVAudioSessionCategoryPlayAndRecord;
+                NSString* sessionMode = AVAudioSessionModeVideoCall;
+                AVAudioSessionCategoryOptions options = AVAudioSessionCategoryOptionDefaultToSpeaker | AVAudioSessionCategoryOptionAllowBluetooth | AVAudioSessionCategoryOptionDuckOthers;
                 [self.avSession setCategory:sessionCategory mode:sessionMode options:options error:&err];
                 if (![self.avSession setActive:YES error:&err]) {
                     // other audio with higher priority that does not allow mixing could cause this to fail
