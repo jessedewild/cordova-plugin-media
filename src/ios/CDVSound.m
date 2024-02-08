53c937dd92e200a90094f5a9ead6cd25c630e424
Jesse de Wild <jessedw@outlook.com>
2024-02-08 14:47:58 +0100

    Testing more categories

--- src/ios/CDVSound.m
+++ src/ios/CDVSound.m
@@ -438,7 +438,7 @@ BOOL keepAvAudioSessionAlwaysActive = NO;
                     bPlayAudioWhenScreenIsLocked = [playAudioWhenScreenIsLocked boolValue];
                 }
 
-                NSString* sessionCategory = bPlayAudioWhenScreenIsLocked ? AVAudioSessionCategoryMultiRoute : AVAudioSessionCategoryAmbient;
+                NSString* sessionCategory = bPlayAudioWhenScreenIsLocked ? AVAudioSessionCategoryPlayback : AVAudioSessionCategorySoloAmbient;
                 [self.avSession setCategory:sessionCategory error:&err];
                 if (![self.avSession setActive:YES error:&err]) {
                     // other audio with higher priority that does not allow mixing could cause this to fail
